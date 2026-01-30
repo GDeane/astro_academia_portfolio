45a5823453266b385876a9a89d8e149fcd0845df
gavindeane <gavindeane1@gmail.com>
2026-01-30 15:01:19 -0500

    Updated my profile details, added research interests, and chose the website theme.

--- src/settings.ts
+++ src/settings.ts
@@ -1,20 +1,39 @@
 export const profile = {
-	fullName: 'Your Name',
-	title: '',
-	institute: '',
-	author_name: '', // Author name to be highlighted in the papers section
+	fullName: 'Gavin Deane',
+	title: 'Computer Science Masters Student',
+	institute: 'University of Waterloo',
+	author_name: 'Gavin Deane', // Author name to be highlighted in the papers section
 	research_areas: [
-		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
+		{
+			title: 'Game Theory',
+			description: 'I enjoy modeling competition and cooperation in games, economics, and society',
+			field: 'Applied Mathematics'
+		},
+		{
+			title: 'MultiAgent Systems',
+			description: 'I am interested in how self-interested agents (AI or human) interact in various settings',
+			field: 'Machine Learning'
+		},
+		{
+			title: 'Theory of Mind',
+			description: 'It fascinates me to think about how humans (and AI systems) model the world',
+			field: 'Cognitive Science'
+		},
+		{
+			title: 'Artificial Intelligence',
+			description: 'I believe that attempting to build and understand AI systems leads us to a greater understanding of our own cognition',
+			field: 'Computer Science'
+		},
 	],
 }
 
 // Set equal to an empty string to hide the icon that you don't want to display
 export const social = {
-	email: '',
-	linkedin: '',
-	x: 'https://www.x.com/',
+	email: 'gdeane@uwaterloo.ca',
+	linkedin: 'https://www.linkedin.com/in/gdeane/',
+	x: '',
 	bluesky: '',
-	github: '',
+	github: 'https://github.com/Gdeane',
 	gitlab: '',
 	scholar: '',
 	inspire: '',
@@ -26,8 +45,8 @@ export const template = {
 	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
-	lightTheme: 'light', // Select one of the Daisy UI Themes or create your own
-	darkTheme: 'dark', // Select one of the Daisy UI Themes or create your own
+	lightTheme: 'emerald', // Select one of the Daisy UI Themes or create your own
+	darkTheme: 'night', // Select one of the Daisy UI Themes or create your own
 	excerptLength: 200,
 	postPerPage: 5,
     base: '' // Repository name starting with /
